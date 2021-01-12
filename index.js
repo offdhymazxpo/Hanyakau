b2f91a24acb480f6594c00d878635b76fa5af347
Ma'ruf <72728486+mrfzvx12@users.noreply.github.com>
2021-01-12 11:48:20 +0700

    Donasi & translate

--- index.js
+++ index.js
@@ -341,7 +341,7 @@ conn.sendMessage(id, `Mau donai agar bot bisa terus berjalan.
  اتَّقوا النَّارَ ولو بشقِّ تمرةٍ ، فمن لم يجِدْ فبكلمةٍ طيِّبةٍ
 _“jauhilah api neraka, walau hanya dengan bersedekah sebiji kurma (sedikit). Jika kamu tidak punya, maka bisa dengan kalimah thayyibah” [HR. Bukhari 6539, Muslim 1016]_
 
-*Pulsa XL :* _${pulsa}_
+*Pulsa :* _${pulsa}_
 *Dana :* _${dana}_
 *OVO :* _${ovo}_`,MessageType.text, { quoted: m } );
 }
